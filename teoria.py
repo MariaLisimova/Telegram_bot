a6fa83b7cce5b95f249c1720b258f02ce62fa6da
MariaLisimova <109218722+MariaLisimova@users.noreply.github.com>
2022-07-14 16:49:54 +0300

    Add files via upload

---
+++ teoria.py
@@ -0,0 +1,33 @@
+
+
+
+num_int = 5
+num_float = 5.5
+
+first_name = 'Artem'
+last_name = 'Ivanov'
+
+
+# print(num_int)
+# print(name)
+# print(name, num_int)
+
+# print(num_int + num_
+
+# print(num_int/num_float)
+
+# if num_int > 4
+#     print(f' Число(num_int)>4')
+# elif num_int> 10
+#     print(num_int > 10)
+
+# for letter in first_name;
+    # print (letter)
+
+num_list = (1, 2, 3, 4, 5)
+
+while num_int < 10:
+    num_int += 1
+    if num_int == 8:
+        break
+    print(num_int)
